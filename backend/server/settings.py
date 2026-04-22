d7d36a37bdacc98551f3d25ae675b78b7cdcc002
wincodes <otokinaodafe@gmail.com>
2026-04-22 12:58:57 +0100

    allow all hosts for deployment

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--i^5bam1xi!k^$hyanp@-1kgey0aciz8=i55n@-pn5^!9jl8_c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
